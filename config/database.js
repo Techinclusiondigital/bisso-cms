5bad88cabca44d5fe2de8061d05aeeda710c3c7c
«sonted80» <sonted@alu.edu.gva.es>
2025-07-31 22:39:02 +0200

    cansancio

--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
+// path: config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
-    connection: {
-      host:     env('DATABASE_HOST'),
-      port:     env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user:     env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl:      env.bool('DATABASE_SSL', false),
-    },
+    client: 'postgres',
+    // Le pasamos directamente la URL completa de la DB
+    connection: env('DATABASE_URL'),
     debug: false,
   },
 });
