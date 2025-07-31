4498300246c3d1f475ae59fbd7871fda1ba2afb1
«sonted80» <sonted@alu.edu.gva.es>
2025-07-31 19:56:27 +0200

    strapi usuario

--- config/server.js
+++ config/server.js
@@ -1,10 +1,14 @@
-// path: config/server.js
+// path: bisso-cms/config/server.js
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),               // que escuche en todas las interfaces
-  port: env.int('PORT', 1337),               // usa process.env.PORT o 1337 por defecto
+  host: env('HOST', '0.0.0.0'),         // escucha en todas las IPs
+  port: env.int('PORT', 1338),         // puerto por defecto
   app: {
+    // Aquí van tus APP_KEYS:
     keys: env.array('APP_KEYS', [
-      'keya', 'keyb', 'keyc', 'keyd'
+      'replace_me_with_a_long_random_string_1',
+      'replace_me_with_a_long_random_string_2',
+      'replace_me_with_a_long_random_string_3',
+      'replace_me_with_a_long_random_string_4',
     ]),
   },
 });
