abb2959a86ca86d3fece4223a45724b7b006b076
«sonted80» <sonted@alu.edu.gva.es>
2025-07-31 21:33:45 +0200

    claves

--- config/server.js
+++ config/server.js
@@ -1,16 +1,19 @@
-// path: bisso-cms/config/server.js
+// <<< DEBUG INICIO >>>
+console.log('>>> ENV.APP_KEYS:', process.env.APP_KEYS);
+console.log('>>> ENV.JWT_SECRET:', process.env.JWT_SECRET);
+console.log('>>> process.env:', Object.keys(process.env).filter(k => /APP_KEYS|SECRET/.test(k)));
+// <<< DEBUG FIN >>>
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
   app: {
-    keys: env
-      .array('APP_KEYS', [
-        // estos valores sólo se usan si NO encuentras APP_KEYS en env
-        'replace_me_with_a_long_random_string_1',
-        'replace_me_with_a_long_random_string_2',
-        'replace_me_with_a_long_random_string_3',
-        'replace_me_with_a_long_random_string_4',
-      ]),
+    keys: env.array('APP_KEYS', [
+      'replace_me_with_a_long_random_string_1',
+      'replace_me_with_a_long_random_string_2',
+      'replace_me_with_a_long_random_string_3',
+      'replace_me_with_a_long_random_string_4',
+    ]),
   },
 });
 
