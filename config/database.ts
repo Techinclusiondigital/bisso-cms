e708840cc12f8735064d4fedaf981f07cbcb53ab
«sonted80» <sonted@alu.edu.gva.es>
2025-08-01 18:58:52 +0200

    database1

--- config/database.ts
+++ config/database.ts
@@ -4,7 +4,9 @@ export default ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
