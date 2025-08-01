3da70f5bca5ebfb2c13bf00d57651777b1ffee05
«sonted80» <sonted@alu.edu.gva.es>
2025-08-01 16:01:04 +0200

    no funciona

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,8 @@
-module.exports = ({ env }) => {
-  console.log("admin.js loaded!", env('ADMIN_JWT_SECRET'));
-  return {
-    apiToken: { salt: env('API_TOKEN_SALT') },
-    auth: { secret: env('ADMIN_JWT_SECRET') },
-  };
-};
+module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+});
