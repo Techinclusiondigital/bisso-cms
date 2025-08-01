e74e86569d42b7dc221745302b7430876fc37ddf
«sonted80» <sonted@alu.edu.gva.es>
2025-08-01 02:24:16 +0200

    ninne

--- config/admin.js
+++ config/admin.js
@@ -1,12 +1,12 @@
-// path: ./config/admin.ts
-export default ({ env }) => ({
-  // Para los API Tokens (Plugin users-permissions)
+// path: ./config/admin.js
+module.exports = ({ env }) => ({
+  // Configuración del plugin de API Tokens:
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'localDevSaltChangeMe123!'),
+    salt: env('API_TOKEN_SALT', 'tz85/mGLia8jyPhizJ8mhA=='),
   },
-  // Para la autenticación del panel Admin (JWT interno)
+  // Configuración del JWT interno del Admin Panel:
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'localDevAuthSecretChangeMe456!'),
+    secret: env('ADMIN_JWT_SECRET', 'nXthY9anDiSTXWfG8pVL0w=='),
   },
 });
 
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 // path: ./config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
 
   app: {
     // Si existe APP_KEYS en tu entorno, lo lee; si no, usa este array de respaldo
